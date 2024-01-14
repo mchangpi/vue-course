250f16b8ec62c90b8cbe8d728f5c25f5f088b370
Milton Chang <mchangpi@gmail.com>
2024-01-14 21:32:44 +0800

    L17 Reactive Objects

--- 7-composition-basics/src/views/HomeView.vue
+++ 7-composition-basics/src/views/HomeView.vue
@@ -1,29 +1,39 @@
 <template>
   <div class="flex flex-col items-center gap-4">
-    <h3>{{ counterTitle }}:</h3>
+    <h3>{{ counterData.title }}</h3>
     <div class="mt-2 space-x-2 flex justify-center">
       <button @click="decreaseCounter" class="border-2 px-4">-</button>
-      <span class="counter">{{ counter }}</span>
+      <!-- <span class="counter">{{ counter }}</span> -->
+      <span class="counter">{{ counterData.count }}</span>
       <button @click="increaseCounter" class="border-2 px-4">+</button>
     </div>
     <div class="edit">
       <h2>Edit Counter Title:</h2>
-      <input v-model="counterTitle" type="text" />
+      <!-- <input v-model="counterTitle" type="text" /> -->
+      <input v-model="counterData.title" type="text" />
     </div>
   </div>
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { reactive } from "vue";
+/*
 const counter = ref(0);
 const counterTitle = ref("My Counter");
+*/
+const counterData = reactive({
+  count: 0,
+  title: "My Counter",
+});
 
 const decreaseCounter = () => {
-  counter.value--;
+  // counter.value--;
+  counterData.count--;
 };
 
 const increaseCounter = () => {
-  counter.value++;
+  // counter.value++;
+  counterData.count++;
 };
 </script>
 
