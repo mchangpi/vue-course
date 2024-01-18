46456da20f2b6b4a1ff95ae3c27d48c6d14c7c3b
Milton Chang <mchangpi@gmail.com>
2024-01-18 11:30:14 +0800

    L42 Teleport - Part 2

--- 7-composition-basics/src/views/ModalsView.vue
+++ 7-composition-basics/src/views/ModalsView.vue
@@ -2,15 +2,17 @@
   <div class="modals flex flex-col items-center gap-2 sp">
     <h1>Modals</h1>
     <button @click="toggleModal" class="border-2 p-2">Show/Hide modal</button>
-    <div v-if="showModal" class="bg-slate-100">
-      <h1>This is a modal</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium
-        repudiandae, numquam natus inventore sequi nesciunt voluptatem iure
-        quisquam placeat, nostrum nisi! Sit aperiam quam, beatae consectetur
-        quos saepe eligendi blanditiis.
-      </p>
-    </div>
+    <Teleport to=".modals-container">
+      <div v-if="showModal" class="bg-slate-100 z-10">
+        <h1>This is a modal</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium
+          repudiandae, numquam natus inventore sequi nesciunt voluptatem iure
+          quisquam placeat, nostrum nisi! Sit aperiam quam, beatae consectetur
+          quos saepe eligendi blanditiis.
+        </p>
+      </div>
+    </Teleport>
   </div>
 </template>
 
