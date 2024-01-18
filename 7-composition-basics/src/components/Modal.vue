263f3923e8e4e3ae40173ecce88d62c5cfc57df1
Milton Chang <mchangpi@gmail.com>
2024-01-18 16:22:20 +0800

    L46 Slots: <slot> and v-slot

--- 7-composition-basics/src/components/Modal.vue
+++ 7-composition-basics/src/components/Modal.vue
@@ -1,17 +1,19 @@
 <template>
   <Teleport to=".modals-container">
     <div class="z-10 bg-slate-100">
-      <h1>This is a modal</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium
-        repudiandae, numquam natus inventore sequi nesciunt voluptatem iure
-        quisquam placeat, nostrum nisi! Sit aperiam quam, beatae consectetur
-        quos saepe eligendi blanditiis.
-      </p>
+      <h1><slot name="title" /></h1>
+      <slot />
+
+      <!-- <pre>{{ $slots.title() }}</pre> -->
     </div>
   </Teleport>
 </template>
 
 <script setup>
-/* */
+/*
+import { useSlots } from 'vue';
+
+const slots = useSlots();
+console.log(slots.title());
+*/
 </script>
--- 7-composition-basics/src/views/ModalsView.vue
+++ 7-composition-basics/src/views/ModalsView.vue
@@ -2,7 +2,17 @@
   <div class="modals sp flex flex-col items-center gap-2">
     <h1>Modals</h1>
     <button @click="toggleModal" class="border-2 p-2">Show/Hide modal</button>
-    <Modal v-if="showModal"></Modal>
+    <Modal v-if="showModal">
+      <!-- <template #title>My Title (with shorthand #)</template> -->
+      <template v-slot:title>My Title</template>
+
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium
+        repudiandae, numquam natus inventore sequi nesciunt voluptatem iure
+        quisquam placeat, nostrum nisi! Sit aperiam quam, beatae consectetur
+        quos saepe eligendi blanditiis.
+      </p>
+    </Modal>
   </div>
 </template>
 
