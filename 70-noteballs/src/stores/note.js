aae4863a3a6d5f80055ce099064d2723b792f226
Milton Chang <mchangpi@gmail.com>
2024-01-24 06:59:05 +0800

    L92 Install Pinia & Setup

---
+++ 70-noteballs/src/stores/note.js
@@ -0,0 +1,29 @@
+import { ref, computed } from 'vue';
+import { defineStore } from 'pinia';
+
+export const useNoteStore = defineStore('note', () => {
+  const initNoteArr = [
+    { id: 1, content: 'note content 1' },
+    { id: 2, content: 'note content 2' },
+  ];
+
+  const noteArr = ref(initNoteArr);
+
+  /* // getters
+  const oddOrEven = computed(() => {
+    if (count.value % 2 === 0) return 'even';
+    else return 'odd';
+  });
+  */
+
+  /* 
+  // actions
+  function increaseCounter(step) {
+    count.value += step;
+  }
+  function decreaseCounter(step) {
+    count.value -= step;
+  }*/
+
+  return { noteArr };
+});
